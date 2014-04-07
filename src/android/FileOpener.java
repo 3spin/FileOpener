e834b474ecc9a9601137f8dc672656fa9833f393
Miguel dos Santos Vaz Dias Wicht <mdw@da1mac3.3spin.corp>
2014-04-07 12:25:10 +0200

    open files from res folder

--- src/android/FileOpener.java
+++ src/android/FileOpener.java
@@ -11,6 +11,13 @@ package com.phonegap.plugins.fileopener;
 import java.io.IOException;
 import java.net.URLConnection;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import android.content.res.Resources;
+
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -40,10 +47,26 @@ public class FileOpener extends CordovaPlugin {
         }
         return false;
     }
+    
+    /**
+     * The URI for a file path.
+     *
+     * @param {String} path
+     *      The given path to the file
+     *
+     * @return The URI pointing to the given path
+     */
+    private Uri getUriForPath(String path) {
+        if (path.startsWith("resource://")) {
+            return getUriForResource(path);
+        }
+        
+        return Uri.parse(path);
+    }
 
     private void openFile(String url) throws IOException {
         // Create URI
-        Uri uri = Uri.parse(url);
+        Uri uri = getUriForPath(url);
 
         Intent intent;
         // Check what kind of file you are trying to open, by comparing the url with extensions.
@@ -111,5 +134,63 @@ public class FileOpener extends CordovaPlugin {
 
         this.cordova.getActivity().startActivity(intent); // TODO handle ActivityNotFoundException
     }
+    
+    /**
+     * The URI for a relative path based on the res folder
+     *
+     * @param {String} path
+     *      The given relative path
+     *
+     * @return The URI pointing to the given path
+     */
+    private Uri getUriForResource(String path) {
+        String resPath      = path.replaceFirst("resource://", "");
+        String directory    = resPath.substring(0, resPath.lastIndexOf('/'));
+        String fileName     = resPath.substring(resPath.lastIndexOf('/') + 1);
+        String resName      = fileName.substring(0, fileName.lastIndexOf('.'));
+        String extension    = resPath.substring(resPath.lastIndexOf('.'));
+        String storage      = cordova.getActivity().getExternalCacheDir().toString() + "/file_opener";
 
+        Resources res       = cordova.getActivity().getResources();
+        int resId           = res.getIdentifier(resName, directory, cordova.getActivity().getPackageName());
+        File file           = new File(storage, resName + extension);
+
+        if (resId == 0) {
+            System.err.println("Resource not found: " + resPath);
+        }
+
+        new File(storage).mkdir();
+        
+        try {
+            FileOutputStream outStream  = new FileOutputStream(file);
+            InputStream inputStream     = res.openRawResource(resId);
+            
+            copyFile(inputStream, outStream);
+            outStream.flush();
+            outStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return Uri.fromFile(file);
+    }
+    
+    /**
+     * Writes an InputStream to an OutputStream
+     *
+     * @param {InputStream} in
+     * @param {OutputStream} out
+     *
+     * @return void
+     */
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+    
 }
